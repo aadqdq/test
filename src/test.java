e3f773804cff47aafd27d51bd2fddb7334234c7b
aadqdq <2665392131@qq.com>
2026-01-30 21:36:03 +0800

    修改了个七

--- src/test.java
+++ src/test.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 import java.util.Random;
 public class test {
     public static void main(String[] args) {
-        System.out.println("123456");
+        System.out.println("1234567");
+
         /*System.out.println("Hello World");
 
         Scanner sc=new Scanner(System.in);
